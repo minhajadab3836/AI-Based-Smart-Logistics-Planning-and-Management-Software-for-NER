bf819dc684203d638b1fbf365d58a028dd95ca51
shahin5sarkar-droid <shahin5sarkar@gmail.com>
2026-08-28 23:23:02 +0530

    style: substitute map tiles with standard OpenStreetMap

--- mobile-app/src/components/DriverMap.tsx
+++ mobile-app/src/components/DriverMap.tsx
@@ -98,8 +98,8 @@ export const DriverMap: React.FC<DriverMapProps> = ({
       zoomControl={false}
     >
       <TileLayer
-        attribution='&copy; CARTO'
-        url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
 
       {/* 1. STARTING CITY MARKER: GUWAHATI (GREEN) */}
